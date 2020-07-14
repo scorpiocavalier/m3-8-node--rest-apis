a057dfe2a7380f49e7c1db8c2cd6c02d08cb62a0
Arturo Chen <art.can.fly@gmail.com>
2020-07-14 16:14:56 -0400

    E2 done

--- handlers/clientHandlers.js
+++ handlers/clientHandlers.js
@@ -1,4 +1,40 @@
 const { v4: uuidv4 } = require('uuid');
 const { clients } = require('../data/clients');
 
-// write your handlers here...
+const handleRoot = (req, res) => {
+  res.status(200).send('root')
+}
+
+const handleClients_Get = (req, res) => {
+  res.status(200).json(clients)
+}
+
+const handleClient_Get = (req, res) => {
+  const { id } = req.params
+  client = clients.filter(client => id === client.id)
+  res.status(200).json(client)
+}
+
+const handleClient_Post = (req, res) => {
+  const { email } = req.body
+  let alreadyExists = clients.some(client => email === client.email)
+  if(!alreadyExists) {
+    const id = uuidv4()
+    res.status(201).json({ id, ...req.body })
+  } else {
+    res.status(304).send("A user with this email already exists.")
+  }
+}
+
+const handleClient_Delete = (req, res) => {
+  const { id } = req.params
+  const index = clients.indexOf(client => id === client.id)
+  clients.splice(index, 1)
+  res.status(200).json(clients)
+}
+
+module.exports = {
+  handleRoot,
+  handleClients_Get, handleClient_Get,
+  handleClient_Post, handleClient_Delete
+}
--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express       = require('express')
+const bodyParser    = require('body-parser')
+const morgan        = require('morgan')
+const {
+  handleRoot,
+  handleClients_Get, handleClient_Get,
+  handleClient_Post, handleClient_Delete
+}                   = require('./handlers/clientHandlers')
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*')
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
+    )
+    next()
   })
   .use(morgan('tiny'))
   .use(express.static('public'))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
-  // endpoints
+  .get('/', handleRoot)
+  .get('/clients', handleClients_Get)
+  .get('/clients/:id', handleClient_Get)
+  .post('/clients', handleClient_Post)
+  .delete('/clients/:id', handleClient_Delete)
 
-  .listen(8000, () => console.log(`Listening on port 8000`));
+  .listen(8000, () => console.log(`Listening on port 8000`))
